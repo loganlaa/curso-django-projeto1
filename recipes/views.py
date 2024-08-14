ca8de6cb5301321cda7bc6cfa9f64367c53db6c6
Logan <logan.alvima@gmail.com>
2024-08-14 13:39:33 -0300

    Exemplos de templates temp

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render   #lë um arquivo e renderiza ele
 from django.http import HttpResponse
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("HOME!")
+    return render(request, 'recipes/home.html', status=200, context={
+        'name' : "Logan"
+    })
 
 def sobre(request):
-    return HttpResponse("SOBRE!")
+    return render(request, 'me-apague/temp.html')
 
 def contato(request):
     return HttpResponse("CONTATO!")

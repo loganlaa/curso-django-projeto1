dde2045b313e56aa4c3033b08398940e24cdd777
Logan <logan.alvima@gmail.com>
2024-08-14 15:02:07 -0300

    Removido Contato e Sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render   #lë um arquivo e renderiza ele
-from django.http import HttpResponse
 
 
 # Create your views here.
@@ -8,9 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', status=200, context={
         'name' : "Logan"
     })
-
-def sobre(request):
-    return HttpResponse("SOBRE!")
-
-def contato(request):
-    return render(request, 'recipes/contato.html')

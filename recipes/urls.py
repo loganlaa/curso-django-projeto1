cf0f2b2ca6892b4e040733694916284f9133b3ba
Logan <logan.alvima@gmail.com>
2024-08-18 16:23:19 -0300

    dinamizando a url, com parametros passados pelo user

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from recipes.views import home
+from . import views #importando o módulo views
 
 
 urlpatterns = [
-    path('', home),
+    path('', views.home), #Usando a funcao homer que está dentro de views
+    path('recipes/<int:id>/', views.recipe), #Recebe o parametro "id" ao acionar a view, que é passado com ela junto com a request, deixando a url dinamica
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', status=200, context={
         'name': "Logan"
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', status=200, context={
+        'name': "Logan"
+    })

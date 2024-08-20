779b42a2af631ca447435c0fe369d1531bd84569
Logan <logan.alvima@gmail.com>
2024-08-20 20:31:46 -0300

    Adicionando o argumento name no path do urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views  #importando o módulo views
+from . import views #importando o módulo views
+
 
 urlpatterns = [
-    path('', views.home, name="recipes-home"),  #Usando a funcao homer que está dentro de views
-    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
-    #Recebe o parametro "id" ao acionar a view, que é passado com ela junto com a request, deixando a url dinamica
+    path('', views.home, name="recipes-home"), #Usando a funcao homer que está dentro de views
+    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"), #Recebe o parametro "id" ao acionar a view, que é passado com ela junto com a request, deixando a url dinamica
 ]
-# "name" dá nomes únicos para as urls que podem ser referenciadas por {% url 'name' %}

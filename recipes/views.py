091371ae6ee8718e7d696cbc10d1006d0cb007b8
Logan <logan.alvima@gmail.com>
2024-08-20 16:17:28 -0300

    Usand for com variaveis definidas por context na view, gerando dados aleatorios para preencher automaticamente as partials das paginas.

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render  #lë um arquivo e renderiza ele
-
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', status=200, context={
-        'name': "Logan"
+    return render(request, 'recipes/pages/home.html', status=200, context={ #context é importante para passar variáveis aplicadas somente ä pagina
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', status=200, context={
-        'name': "Logan"
+        'recipe': make_recipe(),
+        'is_detail_page' : True,
     })
